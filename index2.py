af41885ec13c5ee92659fcd0311ece74d6a4eb55
Aaron W Chen <awchen2009@gmail.com>
2019-08-07 16:23:04 -0700

    Add new style testing files and update notebook cells

---
+++ index2.py
@@ -0,0 +1,10 @@
+stuff = [1, 2, 3, 4]
+
+
+def totally(x):
+    return x + "-totally"
+
+
+things = {
+    ";asdfljasldkjfals;kdjf;laskdjfl;ksajdf;lkajsdf;ljas;dlfj;lasjdf;lajsd;lfjas;lkdjfkl;asjdkl;fjas;kldjfl;asdjfl;asdjkfl;askjdfl;aksjdfl;akjsdl;fkasjd;fklajsndkvnsl.khjgnal.nvldsnfg": 1
+}
